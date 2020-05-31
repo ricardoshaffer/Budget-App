97b8e3c73f970a9ee7c0cad0fb70b0be29a43600
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-05-31 15:21:54 -0400

    added mongoDB connection line to server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ mongoose.connect("mongodb://localhost/budget" || "mongodb://localhost/budget", {
 
 // routes
 app.use(require("./routes/api.js"));
-
+mongoose.connect(MONGODB_URI);
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
